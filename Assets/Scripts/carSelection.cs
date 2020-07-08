414d2b53590f009d314be38e481601c0f64e8ad1
Jeffrey Huang <jeffvh@outlook.com>
2020-07-08 21:27:49 +1000

    init project with cars that grow and rotate on platform. first car in list is always selected first

---
+++ Assets/Scripts/carSelection.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class carSelection : MonoBehaviour
+{
+    private GameObject[] carList;
+
+    // Start is called before the first frame update
+    void Start() {
+        carList = new GameObject[transform.childCount];
+
+        // Loop through the child items and fill the list in the correct slots
+        for (int i = 0; i < transform.childCount; ++i) {
+            carList[i] = transform.GetChild(i).gameObject;
+        }
+
+        // Deactivate all the gameObjects in the list
+        foreach(GameObject gameObj in carList) {
+            gameObj.SetActive(false);
+        }
+
+        // Set the initial GO (GameObject) to be active
+        if (carList[0]) {
+            carList[0].SetActive(true);
+        }
+    }
+
+    // Update is called once per frame
+    void Update() {
+        
+    }
+}
