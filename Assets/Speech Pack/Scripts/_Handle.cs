1bb032af9f0340ba0c81e1487d760aa6da663180
Jeffrey Huang <jeffvh@outlook.com>
2020-07-11 19:14:54 +1000

    update model and handling of wit ai api response

--- Assets/Speech Pack/Scripts/_Handle.cs
+++ Assets/Speech Pack/Scripts/_Handle.cs
@@ -1,73 +1,89 @@
-﻿using UnityEngine;
-using System.Collections;
-using UnityEngine.UI;
+﻿using Newtonsoft.Json;
 using System.Collections.Generic;
-using System;
-using Newtonsoft.Json;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
 
 //Custom 8
-public partial class Wit3D : MonoBehaviour {
+public partial class Wit3D : MonoBehaviour
+{
+    public Text myHandleTextBox;
+    private bool actionFound = false;
 
-	public Text myHandleTextBox;
-	private bool actionFound = false;
+    void Handle(string jsonString)
+    {
 
-	void Handle (string jsonString) {
-		
-		if (jsonString != null) {
+        if (jsonString != null)
+        {
 
-			RootObject theAction = new RootObject ();
-			Newtonsoft.Json.JsonConvert.PopulateObject (jsonString, theAction);
+            RootObject theAction = new RootObject();
+            JsonConvert.PopulateObject(jsonString, theAction);
 
-			if (theAction.entities.open != null) {
-				foreach (Open aPart in theAction.entities.open) {
-					Debug.Log (aPart.value);
-					myHandleTextBox.text = aPart.value;
-					actionFound = true;
-				}
-			}
-			if (theAction.entities.close != null) {
-				foreach (Close aPart in theAction.entities.close) {
-					Debug.Log (aPart.value);
-					myHandleTextBox.text = aPart.value;
-					actionFound = true;
-				}
-			}
+            GetIntent(theAction);
 
-			if (!actionFound) {
-				myHandleTextBox.text = "Request unknown, please ask a different way.";
-			} else {
-				actionFound = false;
-			}
+            if (!actionFound)
+            {
+                myHandleTextBox.text = "Request unknown, please ask a different way.";
+            }
+            else
+            {
+                actionFound = false;
+            }
 
- 		}//END OF IF
+        }//END OF IF
 
- 	}//END OF HANDLE VOID
+    }
+
+    void GetIntent(RootObject rootObject)
+    {
+        if (rootObject == null || rootObject.intents == null || rootObject.intents.Count < 1)
+        {
+            Debug.Log("No intent found");
+            return;
+        }
+
+        myHandleTextBox.text = rootObject.intents.First().name.Replace('_', ' ');
+        actionFound = true;
+    }//END OF HANDLE VOID
 
 }//END OF CLASS
-	
+
 
 //Custom 9
-public class Open {
-	public bool suggested { get; set; }
-	public double confidence { get; set; }
-	public string value { get; set; }
-	public string type { get; set; }
+public class PotentialEntity
+{
+    public string body { get; set; }
+    public double confidence { get; set; }
+    public int end { get; set; }
+    public List<object> entities { get; set; }
+    public string id { get; set; }
+    public string name { get; set; }
+    public string role { get; set; }
+    public int start { get; set; }
+    public string type { get; set; }
+    public string value { get; set; }
 }
 
-public class Close {
-	public bool suggested { get; set; }
-	public double confidence { get; set; }
-	public string value { get; set; }
-	public string type { get; set; }
+public class Entities
+{
+    [JsonProperty("open:open")]
+    public List<PotentialEntity> open { get; set; }
+
+    [JsonProperty("close:close")]
+    public List<PotentialEntity> close { get; set; }
 }
 
-public class Entities {
-	public List<Open> open { get; set; }
-	public List<Close> close { get; set; }
+public class Intent
+{
+    public double confidence { get; set; }
+    public string id { get; set; }
+    public string name { get; set; }
 }
 
-public class RootObject {
-	public string _text { get; set; }
-	public Entities entities { get; set; }
-	public string msg_id { get; set; }
+public class RootObject
+{
+    public string text { get; set; }
+    public Entities entities { get; set; }
+    public List<Intent> intents { get; set; }
+    public object traits { get; set; }
 }
