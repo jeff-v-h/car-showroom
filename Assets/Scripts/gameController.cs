af78f4ad6baa30c92ad0f4c6f89eb9cd3e250608
Jeffrey Huang <jeffvh@outlook.com>
2020-07-16 21:16:36 +1000

    be able to change porsche colour

--- Assets/Scripts/gameController.cs
+++ Assets/Scripts/gameController.cs
@@ -3,7 +3,7 @@ using UnityEngine.SceneManagement;
 
 public class gameController : MonoBehaviour
 {
-    public static string currentSelectedCar = "myLamboConvert";
+    public static string currentSelectedCar = "myPorsche";
 
     // Start is called before the first frame update
     void Start()
