6338eb16c170263533befd07a95f9c13460195b1
Jeffrey Huang <jeffvh@outlook.com>
2020-07-21 21:01:52 +1000

    add video and engine as well as hood animation for porsche

--- Assets/Scripts/gameController.cs
+++ Assets/Scripts/gameController.cs
@@ -4,7 +4,7 @@ using UnityEngine.SceneManagement;
 public class gameController : MonoBehaviour
 {
     //public static string currentSelectedCar = "myLamboConvert";
-    public static string currentSelectedCar = "myDownloadedLambo";
+    public static string currentSelectedCar = "myPorsche";
 
     // Start is called before the first frame update
     void Start()
